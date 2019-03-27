4d20eccb085e0c6dbe655300299b9f9c80ffd501
Code4 PC <michaelanablarley@gmail.com>
2019-03-27 02:20:01 +0000

    Second Commit

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -16,6 +16,7 @@
 package com.google.firebase.udacity.friendlychat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -35,6 +36,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -44,6 +46,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-    private static final int RC_SIGN_IN = 1;
+    private static final int RC_SIGN_IN =1;
+    private static final int RC_PHOTO_PICKER =2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -85,8 +89,11 @@ public class MainActivity extends AppCompatActivity {
 
         mfirebasedatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+         mFirebaseStorage=FirebaseStorage.getInstance();
         mMessagesDataReference = mfirebasedatabase.getReference().child("messages");
-        mFirebaseStorage=FirebaseStorage.getInstance();
+        mChatPhotoStorageReference= mFirebaseStorage.getReference().child("chat_photos");
+        
+
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
@@ -107,6 +114,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: Fire an intent to show an image picker
+                Intent intent= new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent,"Complete action using"),RC_PHOTO_PICKER);
+
             }
         });
 
@@ -158,11 +170,8 @@ public class MainActivity extends AppCompatActivity {
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
                                     .setIsSmartLockEnabled(false)
-                                    .setAvailableProviders(Arrays.asList(
-                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
-                                            new AuthUI.IdpConfig.EmailBuilder().build(),
-                                            new AuthUI.IdpConfig.PhoneBuilder().build(),
-                                            new AuthUI.IdpConfig.AnonymousBuilder().build()))
+                                    .setProviders(AuthUI.EMAIL_PROVIDER,
+                                                  AuthUI.GOOGLE_PROVIDER)
                                     .build(),
                             RC_SIGN_IN);
                 }
@@ -250,15 +259,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if (requestCode==RC_SIGN_IN){
-            if(resultCode==RESULT_OK){
-                Toast.makeText(this,"Signed In",Toast.LENGTH_SHORT).show();
-            }else if (resultCode==RESULT_CANCELED){
-                Toast.makeText(this,"Sign In Canceled",Toast.LENGTH_SHORT).show();
+        if (requestCode==RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign In Canceled", Toast.LENGTH_SHORT).show();
                 finish();
             }
+        }else if (requestCode==RC_PHOTO_PICKER && resultCode==RESULT_OK){
+
+                Uri selectImageUri=data.getData();
+                StorageReference photoRef=mChatPhotoStorageReference.child(selectImageUri.getLastPathSegment());
+
+                photoRef.putFile(selectImageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUrl= taskSnapshot.getDownloadUrl();
+                        FriendlyMessage friendlyMessage= new FriendlyMessage(null,mUsername,downloadUrl.toString());
+                        mMessagesDataReference.push().setValue(friendlyMessage);
+                    }
+                });
+                
+                
+            }
         }
-    }
+   
     @Override
     protected void onPause() {
         super.onPause();
